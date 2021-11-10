6f435358f54885ad2caf923d195d4d4de9bd7b07
PastureBedtime <93006698+PastureBedtime@users.noreply.github.com>
2021-11-10 10:32:55 -0500

    v1.1.2

--- TGS.py
+++ TGS.py
@@ -2,7 +2,7 @@
 #Help from - Major
 #
 
-version = '1.1.1'
+version = '1.1.2'
 
 from tkinter import *
 import requests
@@ -53,6 +53,13 @@ def major():
     csp_total = sum(csp_strike_ints)
     csp_len = len(csp_strike)
     csp_mean = csp_total/csp_len
+    
+    sppf = [s['price_filled'] for s in short_puts]
+    list(sppf)
+    sppf_total = sum(sppf)
+    sppf_len = len(sppf)
+    sppf_mean = sppf_total/sppf_len
+    
     #print(f'Short Put Concensus: {int(csp_mean)}')
 
     #
@@ -72,6 +79,13 @@ def major():
     lp_total = sum(lp_strike_ints)
     lp_len = len(lp_strike)
     lp_mean = lp_total/lp_len
+    
+    lppf = [s['price_filled'] for s in long_puts]
+    list(lppf)
+    lppf_total = sum(lppf)
+    lppf_len = len(lppf)
+    lppf_mean = lppf_total/lppf_len
+    
     #print(f'Long Put Concenus: {int(lp_mean)}')
 
     #
@@ -90,6 +104,12 @@ def major():
     cc_total = sum(cc_strike_ints)
     cc_len = len(cc_strike)
     cc_mean = cc_total/cc_len
+    
+    scpf = [s['price_filled'] for s in short_calls]
+    list(scpf)
+    scpf_total = sum(scpf)
+    scpf_len = len(scpf)
+    scpf_mean = scpf_total/scpf_len
     #print(f'Short call Concenus: {int(cc_mean)}')
     
     #
@@ -109,6 +129,13 @@ def major():
     lc_total = sum(lc_strike_ints)
     lc_len = len(lc_strike)
     lc_mean = lc_total/lc_len
+    
+    lcpf = [s['price_filled'] for s in long_calls]
+    list(lcpf)
+    lcpf_total = sum(lcpf)
+    lcpf_len = len(lcpf)
+    lcpf_mean = lcpf_total/lcpf_len
+    
     #print(f'Long call Concenus: {int(lc_mean)}')
 
     #
@@ -136,10 +163,10 @@ def major():
     bullbear=(f'\nSentiment: {bias()}')
     rcheck=(f'\nPrice vs Expected Range: {inrange()}')
     ttr=(f'\n\nTotal Trades Gathered: {totaltrades}')
-    spc=(f' \n\nShort Put Concensus:  {int(csp_mean)} | Number of Trades: {int(csp_len)} | Last 5: {ccl5}')
-    lpc=(f' \nLong Put Concensus:   {int(lp_mean)} | Number of Trades: {int(lp_len)} | Last 5: {lpl5}')
-    scc=(f' \nShort call Concensus: {int(cc_mean)} | Number of Trades: {int(cc_len)} | Last 5: {ccl5}')
-    lcc=(f' \nLong call Concensus:  {int(lc_mean)} | Number of Trades: {int(lc_len)} | Last 5: {lcl5}')
+    spc=(f' \n\nShort Put Concensus:  {int(csp_mean)} | Number of Trades: {int(csp_len)} | AVG Premium: {round(sppf_mean, 2)} | Last 5: {ccl5} ')
+    lpc=(f' \nLong Put Concensus:   {int(lp_mean)} | Number of Trades: {int(lp_len)} | AVG Premium: {round(lppf_mean, 2)} | Last 5: {lpl5}')
+    scc=(f' \nShort call Concensus: {int(cc_mean)} | Number of Trades: {int(cc_len)} | AVG Premium: {round(scpf_mean, 2)}  | Last 5: {ccl5} ')
+    lcc=(f' \nLong call Concensus:  {int(lc_mean)} | Number of Trades: {int(lc_len)} | AVG Premium: {round(lcpf_mean, 2)} | Last 5: {lcl5} ')
     ter=(f' \n\nThetagang Expected Range: {int(put_side)} to {int(call_side)}')
     warn=(f' \n\n\nInformation provided in this tool is not financial advice, and is collected from user input. This input can skew the data unfavorably. Use at own risk.')
     show_me = name + lastp + ttr + bullbear + rcheck + spc + lpc + scc + lcc + ter + warn
@@ -168,7 +195,7 @@ Button(window, text='Gather Data', width=11, command=major).grid(row=3, column=0
 Label (window, text='\nOutput:', bg='black', fg='white', font='none 12 bold').grid(row=5,column=0,sticky=W)
 
 #output textbox
-output = Text(window, width=100, height=20, wrap=WORD, background='#32CD32')
+output = Text(window, width=120, height=20, wrap=WORD, background='#32CD32')
 output.grid(row=6, column=0, columnspan=2,sticky=W)
 
 #how to use
